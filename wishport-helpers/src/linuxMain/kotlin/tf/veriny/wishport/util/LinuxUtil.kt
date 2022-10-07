eb1d8d1d1890eb017bd1d9b5bcd183f3c26f73a1
Lura Skye <l@veriny.tf>
2022-10-07 12:12:59 +0100

    refactor kstrerror into a better function

--- wishport-helpers/src/linuxMain/kotlin/tf/veriny/wishport/util/LinuxUtil.kt
+++ wishport-helpers/src/linuxMain/kotlin/tf/veriny/wishport/util/LinuxUtil.kt
@@ -11,15 +11,33 @@ import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.toKString
 import kotlinx.cinterop.usePinned
 import platform.posix.strerror_r
+import tf.veriny.wishport.EINVAL
+import tf.veriny.wishport.ERANGE
 
-public fun kstrerror(errno: Int): String = memScoped {
-    // this is large, but should always be sufficient
-    // if any strerror is too big, i'll update it
-    val buf = ByteArray(1024)
-    val res = buf.usePinned {
-        strerror_r(errno, it.addressOf(0), buf.size.toULong())
+/**
+ * Converts an integer [errno] into a human-readable string.
+ */
+public fun kstrerror(errno: Int): String = memScoped<String> {
+    var size = 64
+
+    while (true) {
+        val buf = ByteArray(size)
+
+        val res = buf.usePinned {
+            strerror_r(errno, it.addressOf(0), buf.size.toULong())
+        }
+
+        return if (res > 0) {
+            if (res == EINVAL) "Unknown"
+            else if (res == ERANGE) {
+                size *= 2
+                continue
+            } else throw Throwable("strerror_r returned $res, which isn't allowed!")
+        } else {
+            buf.toKString()
+        }
     }
 
-    // le
-    return buf.toKString()
+    // type inference bug?
+    throw Throwable("unreachable")
 }
