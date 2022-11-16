b6481baf166cb001647b44ae3ea023db30d7888d
Lura Skye <l@veriny.tf>
2022-11-16 23:30:53 +0000

    run spotless

--- wishport-core/src/commonTest/kotlin/tf/veriny/wishport/io/net/Test Socket Options.kt
+++ wishport-core/src/commonTest/kotlin/tf/veriny/wishport/io/net/Test Socket Options.kt
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package tf.veriny.wishport.io.net
 
 import tf.veriny.wishport.assertSuccess
@@ -15,5 +21,4 @@ class `Test Socket Options` {
         assertSuccess { sock.setSocketOption(SO_REUSEADDR, true) }
         assertTrue(assertSuccess { sock.getSocketOption(SO_REUSEADDR) })
     }
-
 }
